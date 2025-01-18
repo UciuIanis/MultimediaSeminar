aa3e80ba973bee0602041212ab5e9998116662c2
UciuIanis <uciuianis22@stud.ase.ro>
2025-01-18 18:28:50 +0200

    added model exam

---
+++ Model exam/script.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    var button = document.getElementById("locate")
+    var latitude = document.getElementById("latitude")
+    var longitude = document.getElementById("longitude")
+    button.addEventListener("click", function() {
+        console.log("object");
+    })
+
+    
+}
